24a18e0b2784d9c41d6c1bee5eb3fc781fff7229
Tomnguyen2604 <tomnguyen2604@gmail.com>
2023-04-19 23:22:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <Home />
+            <About />
+            <Skills />
+            <Work />
+            <Contact />
+        </>
+    );
 }
-
 export default App;
